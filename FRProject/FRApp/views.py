5c8430d129a67c2cf728317eff7d6b69557e242a
bhargavp2004 <105142165+bhargavp2004@users.noreply.github.com>
2024-05-16 15:24:03 +0530

    Implemented findFace function

--- FRProject/FRApp/views.py
+++ FRProject/FRApp/views.py
@@ -49,3 +49,42 @@ def saveEncodings(request):
     pickle.dump(encodeListWithStudentIds, file)
     file.close()
     return HttpResponse("Encoding Complete")
+
+def findFace(request, image) :
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    face_locations = face_recognition.face_locations(image)
+    face_landmarks_list = face_recognition.face_landmarks(image)
+    for face_landmarks in face_landmarks_list:
+        for facial_feature in face_landmarks.keys():
+            for point in face_landmarks[facial_feature]:
+                cv2.circle(image, point, 2, (0, 255, 0), 2) 
+    
+    cv2.namedWindow("Faces", cv2.WINDOW_NORMAL)
+    cv2.imshow("Faces", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    face_encodings = face_recognition.face_encodings(image)[0]
+
+
+    file = open(r'D:\B.TECH\6TH SEMESTER\Project\FaceRecognizerPython\Encodings.p', 'rb')
+    encodeListWithStudentIds = pickle.load(file)
+    for encoding, student_id in encodeListWithStudentIds:
+        studentsIds.append(student_id)
+        encodeList.append(encoding)
+
+    match = face_recognition.compare_faces(encodeList, face_encodings)
+    print(match)
+    if match.count(True) :
+        idx = match.index(True)
+        return idx
+    return -1
+
+def main(request) :
+    image = cv2.imread(r'D:\B.TECH\6TH SEMESTER\Project\FaceRecognizerPython\ImagesToCheck\ak.jpg')
+    idx = findFace(request, image)
+    if str(idx) == str(-1) :
+        return HttpResponse("No matching faces")
+    studentId = studentsIds[idx]
+    return HttpResponse(studentId)
+
